30852476b2aea55af51c8342069487d7aa411cc5
Beszmi <46139601+Beszmi@users.noreply.github.com>
2025-07-01 03:16:58 +0200

    gameobject start

--- game_obj.cpp
+++ game_obj.cpp
@@ -3,14 +3,19 @@
 GameObject::GameObject(const string texture, SDL_Renderer* ren, texture_manager* tex_mgr) {
 	renderer = ren;
 	obj_tex = tex_mgr->get_texture(texture);
+	xpos = 0;
+	ypos = 0;
+	SDL_QueryTexture(obj_tex, NULL, NULL, &src_rect.w, &dst_rect.h);
+	src_rect.x = 0;
+	src_rect.y = 0;
 }
 
 void GameObject::update() {
-
+	
 }
 
 void GameObject::render() {
-
+	SDL_RenderCopy(renderer, obj_tex, &src_rect, &dst_rect);
 }
 
 GameObject::~GameObject() {
